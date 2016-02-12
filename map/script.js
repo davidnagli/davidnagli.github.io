dce5b5ce2bca42d7cc39958ff77df71e9b3d0aab
davidnagli <davidnagli@gmail.com>
2016-02-11 22:06:40 -0500

    Map Game: Added Confirm replacing long instructions

--- map/script.js
+++ map/script.js
@@ -97,7 +97,9 @@ window.onload = function () {
 	for (var i = 0; i < vals.length; i++) {
 		appendInputs(vals[i]);
 	}
-	alert("WARNING: THIS GAME DOES NOT ALWAYS WORK ON MOBILE DEVICES. PLEASE ACCESS IT FROM A COMPUTER \n \nTo play enter the US Postal abreviations for as many states as you can. Then press DONE when you are finished \n \n P.S. Check the checkbox in the upper left corner to hide all the states that you don't need to know for Ms.Eizenbaum's class.");
+	//alert("WARNING: THIS GAME DOES NOT ALWAYS WORK ON MOBILE DEVICES. PLEASE ACCESS IT FROM A COMPUTER \n \nTo play enter the US Postal abreviations for as many states as you can. Then press DONE when you are finished \n \n P.S. Check the checkbox in the upper left corner to hide all the states that you don't need to know for Ms.Eizenbaum's class.");
+	var document.getElementById("showHide").checked = confirm("Would you like to hide all states that are unnessary for Ms. Eizenbaum's class?");
+	toggleStates()
 };
 
 function checkFull() {
